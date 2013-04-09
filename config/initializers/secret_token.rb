c535d911ecc858d5f62a1dd18b709873570ddc97
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2013-04-09 16:15:45 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catchytune::Application.config.secret_token = 'f82c20305aff71978350a9d8ebe22da980a1daba9f8dcb6df79c57ed2afbd00920023355dc113a189d6632352933fdf361f414e7e8beb34614c76b8b26decd0f'
