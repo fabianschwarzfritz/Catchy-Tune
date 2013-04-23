b2801734d72d65e9a3d9147e6621f4c9ecfe7cac
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2013-04-23 18:07:29 +0200

    Fix database connection procedure

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,1 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "catchytune-#{Rails.env}"
-
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    MongoMapper.connection.connect if forked
-  end
-end
+MongoMapper.database = Rails.env == "production" ? "catchytune" : "catchytune-#{Rails.env}"
