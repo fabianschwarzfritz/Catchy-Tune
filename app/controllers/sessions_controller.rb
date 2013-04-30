bd243b3d875e89dae75d989122732b14562f53ef
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2013-04-30 15:36:49 +0200

    Use username instead of email in user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
   end
 
   def login_attempt
-    authorized_user = User.authenticate(params[:email], params[:login_password])
+    authorized_user = User.authenticate(params[:username], params[:login_password])
     if authorized_user
       session[:user_id] = authorized_user.id
       flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.email}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,16 @@ require 'bcrypt'
 class User
   include MongoMapper::Document
 
-  attr_accessible :email, :password, :password_confirmation
-  #EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i
+  attr_accessible :username, :password, :password_confirmation
 
-  key :email, String
+  key :username, String
   key :password, String
   key :hashed_password, String
   key :salt, String
 
   before_save :encrypt_password
   after_save :clear_password
-  validates :email,
+  validates :username,
             :presence => true,
             :uniqueness => true,
             :length => {:in => 3..40}
