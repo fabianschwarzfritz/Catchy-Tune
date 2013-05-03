a543894b556681d20d34a8a85bf7ef0d535ba4e5
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2013-05-03 11:53:49 +0200

    Add method to upload file

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -4,6 +4,7 @@ class PlaylistController < ApplicationController
 
   def initialize
     @grid = Grid.new(MongoMapper.database)
+    @fs = GridFileSystem.new(Mongomapper.database)
   end
 
   def stream
@@ -11,8 +12,18 @@ class PlaylistController < ApplicationController
     send_data getsong(id), :filename => "#{id}", :type => "audio/mpeg"
   end
 
+  def upload
+    songid = params[:id]
+    file = IO.read(params[:upload][:file])
+    storesong(id)
+  end
+
   private
   def getsong(id)
     @grid.get(params[:file])
+  end
+
+  def storesong(id)
+    @fs.put(file, :filename => "songid")
   end
 end
