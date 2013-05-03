8a336e79ca8b50c2d1e63550be3ab7e58faf7488
Fabian Schwarz-Fritz <fabian.schwarz-fritz@sap.com>
2013-05-03 11:23:52 +0200

    First implementation of playlist controller

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,4 +1,18 @@
+require 'prawn'
+
 class PlaylistController < ApplicationController
+
+  def initialize
+    @grid = Grid.new(MongoMapper.database)
+  end
+
   def stream
+    id = params[:songid]
+    send_data getsong(id), :filename => "#{id}", :type => "audio/mpeg"
+  end
+
+  private
+  def getsong(id)
+    @grid.get(params[:file])
   end
 end
