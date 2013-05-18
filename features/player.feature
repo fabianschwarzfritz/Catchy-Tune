4f6cc8c21dba2146e2679177e802bd678c16cc14
Michael Müller <Michael.Mueller@hotmail.de>
2013-05-18 15:31:18 +0200

    Playlist scenarios work
    Test only backend
    CT-104 #time 1h

--- features/player.feature
+++ features/player.feature
@@ -6,18 +6,19 @@ Feature: Audio player
   Background:
     Given a user "existinguser" with password "password"
     And I am logged in with user "existinguser" and password "password"
+    And I am on the home page
 
   Scenario: Add a song to the playlist
     Given I have no songs in my playlist
-    When I add a song to the playlist
+    When I add "a song" to my playlist
     Then I should have a song in my playlist
 
-  Scenario: Appending a song to the playlist
-    Given I have a song in my playlist
-    When I add a song to the playlist
-    Then the new song should be played as the last song
+  Scenario: Append a song to the playlist
+    Given I have "a song" in my playlist
+    When I add "another song" to my playlist
+    Then the song "another song" should be the last song in my playlist
 
-  Scenario: Playing a song
-    Given I have a song in my playlist
+  Scenario: Play a song
+    Given I have "a song" in my playlist
     When I press "play"
     Then a song should be played
--- features/step_definitions/player_steps.rb
+++ features/step_definitions/player_steps.rb
@@ -1,23 +1,35 @@
 Given(/^I have no songs in my playlist$/) do
-  pending
+# walk through playlist until you reach the end
+  get('/playlist/next') until
+      get('/playlist/current_song.text').body.lstrip.blank?
 end
 
-Given(/^I have a song in my playlist$/) do
-  pending
+Given(/^I have "([^"]*)" in my playlist$/) do |song_id|
+  post('/playlist/add', :songid => song_id)
 end
 
 
-When(/^I add a song to the playlist$/) do
-  pending
+When(/^I add "([^"]*)" to my playlist$/) do |song_id|
+  post('/playlist/add', :songid => song_id)
 end
 
 
 Then(/^I should have a song in my playlist$/) do
-  pending
+# accept any answer except only whitespaces
+  get('/playlist/current_song.text').body.should_not match(/^\s*$/)
 end
 
-Then(/^the new song should be played as the last song$/) do
-  pending
+Then(/^the song "([^"]*)" should be the last song in my playlist$/) do |song_id|
+  song = get('/playlist/current_song.text').body.lstrip
+  last_song = song
+
+  until song.blank?
+    last_song = song
+    get('/playlist/next')
+    song = get('/playlist/current_song.text').body.lstrip
+  end
+
+  last_song.should == song_id
 end
 
 Then(/^a song should be played$/) do
