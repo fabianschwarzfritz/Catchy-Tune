111f85712fb8a2f4221b077c5b6b82b529a43a37
Michael Müller <Michael.Mueller@hotmail.de>
2013-05-23 14:31:17 +0200

    Prefetch all tracks' artist in one big query
    Halves processing time of search ( > 5 tracks)
    CT-89 #time 30m

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -16,13 +16,25 @@ class SearchController < ApplicationController
     if params[:search]
       @query = params[:search][:query]
 
-      artists = Artist.by_name_contains(@query).to_a
-      tracks = Track.by_name_contains(@query).to_a
+      artists = Artist.by_name_contains(@query).all
+      tracks = Track.by_name_contains(@query).all
 
       @artists_count = artists.length
       @tracks_count = tracks.length
 
       @results = artists + tracks
+
+      # prefetch artist information for tracks
+      if @tracks_count > 0
+        @track_artists = Hash.new
+
+        # collect distinct artist ids from tracks
+        track_artists = (tracks.collect { |track| track.artist_id }).uniq
+        # get all corresponding artists from database
+        track_artists = Artist.where(:_id => track_artists).all
+        # convert the artists array to a hash for easier access
+        track_artists.each { |artist| @track_artists[artist._id] = artist }
+      end
 
       #else render page with defaults or error message
     end
