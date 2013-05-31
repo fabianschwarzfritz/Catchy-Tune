a6f3842618814d553eb734eff6c642e056429193
Michael Müller <Michael.Mueller@hotmail.de>
2013-05-31 18:05:48 +0200

    Enable pjax for all links unless specified not to pjax it
    Increase the timeout for ajax requests to 5 seconds
    CT-111 #time 20m

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,7 +1,7 @@
 //= require jquery.pjax
 
 $(function () {
-    $(document).pjax('a[data-pjax]', '#pjax-container');
+    $(document).pjax('a[data-pjax!="false"]:not([data-no-pjax]', '#pjax-container', {timeout:5000});
     $(document).on('submit', 'form[data-pjax]', function (event) {
         $.pjax.submit(event, '#pjax-container');
     });
