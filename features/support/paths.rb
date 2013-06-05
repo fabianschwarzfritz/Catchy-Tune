1ab79bed8de8c64394f380b31ec8f88e46be6206
Michael Müller <Michael.Mueller@hotmail.de>
2013-06-05 13:58:47 +0200

    Add file upload in test
    CT-94 #time 35m #resolve

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -2,32 +2,41 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-      when /the root\s?page/i
+      when /root\s?page/i
         '/'
-      when /the sign\s?up\s?page/i
+      when /sign\s?up\s?page/i
         '/users/new'
-      when /the log\s?in\s?page/i
+      when /log\s?in\s?page/i
         '/sessions/login'
-      when /the home\s?page/i
+      when /home\s?page/i
         '/sessions/home'
-      when /the search\s?result\s?page/i
+      when /search\s?result\s?page/i
         '/search/results'
-      when /the register-?artist-?\s?page/i
+      when /register-?artist-?\s?page/i
         '/artists/new'
-      when /the new-track\s?page/i
+      when /new-track\s?page/i
         '/tracks/new'
-      when /the song-?\s?detail\s?page/i
-        /^tracks\S+\//
 
       else
         begin
-          page_name =~ /the (.*) page/
+          page_name =~ /(.*) page/
           path_components = $1.split(/\s+/)
           self.send(path_components.push('path').join('_').to_sym)
         rescue Object => e
           raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
                     "Now, go and add a mapping in #{__FILE__}"
         end
+    end
+  end
+
+  def path_pattern_to(page_name)
+    case (page_name)
+      when /song-?\s?detail[s]?\s?page/i
+        /^\/tracks\/\S+$/
+
+      else
+        raise "Cant't find mapping from \"#{page_name}\" to a path pattern.\n" +
+            "Now, go and add a mappingin #{__FILE__}"
     end
   end
 end
